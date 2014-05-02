9b76f3581d278d6de5e4f070887a5fa35d1d3221
dmvaldman <dmvaldman@gmail.com>
2014-05-02 12:41:56 -0700

    Update Hinge.js

--- Hinge.js
+++ Hinge.js
@@ -1,85 +1,86 @@
 define(function(require, exports, module) {
-    var Utility          = require('famous/utilities/Utility');
-    var OptionsManager   = require('famous/core/OptionsManager');
-    var Modifier         = require('famous/core/Modifier');
     var Transform        = require('famous/core/Transform');
     var Transitionable   = require('famous/transitions/Transitionable');
 
     function Hinge(options) {
-        if (options && options.axis) {
-            options.direction = Hinge.direction[options.axis];
-            options.axis = Hinge.side[options.axis];
-        }
-
         this.options = Object.create(Hinge.DEFAULT_OPTIONS);
-        this.optionsManager = new OptionsManager(this.options);
-
         if (options) this.setOptions(options);
-        
-        this.origin = this.options.direction ? [0.5, this.options.axis] : [this.options.axis, 0.5];
-        this.angle = new Transitionable(this.options.angle);
 
-        _attachModifiers.call(this);
+        // state
+        this._angle  = new Transitionable(this.options.angle);
+        this._origin = _getOriginFromSide.call(this, this.options.side);
     };
 
-    Hinge.side = {
-        1: 1,
-        'left': 0,
-        'top': 0,
-        'right': 1,
-        'bottom': 1
-    };
-
-    Hinge.direction = {
-        1: 1,
-        'left': 0,
-        'right': 0,
-        'top': 1,
-        'bottom': 1
+    Hinge.SIDE = {
+        'TOP'    : 0,
+        'RIGHT'  : 1,
+        'BOTTOM' : 2,
+        'LEFT'   : 3
     };
 
     Hinge.DEFAULT_OPTIONS = {
-        direction: Utility.Direction.Y,
-        axis: 0,
-        angle: 0,
-        transition: false
-    };
-
-    function _attachModifiers() {
-        if (this.options.axis) {
-            this.rotateTransform = function() {
-                return this.options.direction ? Transform.rotateX(this.angle.get()) : Transform.rotateY(this.angle.get());
-            }.bind(this);
-        } else {
-            this.rotateTransform = function() {
-                return this.options.direction ? Transform.rotateX(-this.angle.get()) : Transform.rotateY(-this.angle.get());
-            }.bind(this);
-        }
+        angle : 0,
+        side  : Hinge.SIDE.LEFT,
+        transition : false
     };
 
     Hinge.prototype.setOptions = function setOptions(options) {
-        this.optionsManager.setOptions.apply(this.optionsManager, arguments);
-        return this;
+        if (options.side) _getOriginFromSide.call(this, options.side);
+        if (options.angle) this._angle.set(options.angle);
+        if (options.transition) this.options.transition = options.transition;
     };
 
-    Hinge.prototype.setAngle = function(angle, transition) {
-        transition ? this.angle.set(angle, transition) : this.angle.set(angle, this.options.transition);
+    Hinge.prototype.setAngle = function(angle, transition, callback) {
+        transition = transition || this.options.transition;
+        this._angle.set(angle, transition, callback);
     };
+
+    function _getOriginFromSide(side){
+        var origin;
+        switch (side){
+            case Hinge.SIDE.TOP:
+                origin = [.5,0];
+                break;
+            case Hinge.SIDE.RIGHT:
+                origin = [1,.5];
+                break;
+            case Hinge.SIDE.BOTTOM:
+                origin = [.5,1];
+                break;
+            case Hinge.SIDE.LEFT:
+                origin = [0,.5];
+                break;
+        }
+        return origin;
+    }
+
+    function _getTransformFromAngle(angle){
+        var transform;
+        switch (this.options.side){
+            case Hinge.SIDE.TOP:
+            case Hinge.SIDE.BOTTOM:
+                transform = Transform.rotateX(angle);
+                break;
+            case Hinge.SIDE.RIGHT:
+            case Hinge.SIDE.LEFT:
+                transform = Transform.rotateY(angle);
+                break;
+        }
+        return transform;
+    }
 
     Hinge.prototype.modify = function modify(target) {
-        this.size = target.size[this.options.direction];
+        var transform = _getTransformFromAngle.call(this, this._angle.get());
+        var size = (target.getSize) ? target.getSize() : target.size || null;
         return {
-            transform: this.rotateTransform(),
-            target: {
-                size: target.size,
-                origin: this.origin,
-                target: {
-                    target: target
-                }
+            size : size,
+            target : {
+                origin : this._origin,
+                transform : transform,
+                target : target
             }
-        }
+        };
     };
-
 
     module.exports = Hinge;
 });
