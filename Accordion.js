6120b60f12c79174b33a1f041fde6ee786192b3a
dmvaldman <dmvaldman@gmail.com>
2014-06-30 13:58:57 -0700

    updates to accordion

--- Accordion.js
+++ Accordion.js
@@ -3,27 +3,42 @@ define(function(require, exports, module) {
     var Transitionable   = require('famous/transitions/Transitionable');
 
     function Accordion(options) {
+        // state
+        this._angles = new Transitionable();
+        this._nodes = [];
+        this._initPivotOffset = [0,0,0];
+
         this.options = Object.create(Accordion.DEFAULT_OPTIONS);
         if (options) this.setOptions(options);
-
-        // state
-        this._angles = new Transitionable(this.options.angle);
-        this._nodes = []
     };
 
     Accordion.DIRECTION_X = 0;
     Accordion.DIRECTION_Y = 1;
 
     Accordion.DEFAULT_OPTIONS = {
+        initialAngles : [],
         direction : Accordion.DIRECTION_X,
         transition : false,
-        pivotIndex : 0
+        pivotIndex : 0,
+        reflowSize : false
     };
 
     Accordion.prototype.setOptions = function setOptions(options) {
+        var recomputePivotOffsetFlag = false;
+
+        if (options.initialAngles !== undefined) {
+            this.options.initialAngles = options.initialAngles;
+            this.setAngles(options.initialAngles);
+            recomputePivotOffsetFlag = true;
+        }
         if (options.direction !== undefined) this.options.direction = options.direction;
         if (options.transition !== undefined) this.options.transition = options.transition;
-        if (options.pivotIndex !== undefined) this.options.pivotIndex = options.pivotIndex;
+        if (options.pivotIndex !== undefined) {
+            this.options.pivotIndex = options.pivotIndex;
+            this.options.initialAngles = this._angles.get().slice();
+            recomputePivotOffsetFlag = true;
+        }
+        if (recomputePivotOffsetFlag) _computePivotOffset.call(this);
     };
 
     Accordion.prototype.setAngle = function(index, angle, transition, callback) {
@@ -44,7 +59,30 @@ define(function(require, exports, module) {
 
     Accordion.prototype.sequenceFrom = function(nodes){
         this._nodes = nodes;
+        _computePivotOffset.call(this);
     };
+
+    function _computePivotOffset() {
+        var angles = this.options.initialAngles;
+        var offset = [0,0,0];
+        var nodes = this._nodes;
+        var angle = 0;
+
+        if (!this._nodes.length) return;
+
+        for (var i = 0; i < this.options.pivotIndex; i++){
+            var node = nodes[i];
+            angle = angles[i];
+
+            var size = node.getSize();
+            var length = size[1];
+
+            offset[1] -= Math.cos(angle) * length;
+            offset[2] -= Math.sin(angle) * length;
+        }
+
+        this._initPivotOffset = offset;
+    }
 
     Accordion.prototype.render = function render() {
         var result = [];
@@ -55,43 +93,13 @@ define(function(require, exports, module) {
         var angle = 0;
         var totalLength = 0;
         var maxTotalLength = 0;
-        var pivotLength = 0;
-        var node;
+        var node, pivotOffset;
 
-        for (var i = 0; i < pivotIndex; i++){
-            angle = angles[i];
-            length = nodes[i].getSize()[1];
-            pivotLength += Math.cos(angle) * length;
-        }
+        var offset = [0, 0, 0];
+        for (var i = 0; i < nodes.length; i++){
 
-        var offset = [0, pivotLength, 0];
-        for (var i = pivotIndex - 1; i >= 0; i--){
-            node = nodes[i];
-            angle = angles[i];
+            if (i === pivotIndex) pivotOffset = offset.slice();
 
-            var size = node.getSize();
-            var length = size[1];
-
-            offset[1] -= Math.cos(angle) * length;
-            offset[2] -= Math.sin(angle) * length;
-
-            var transform = Transform.thenMove(Transform.rotateX(angle), offset);
-
-            totalLength += Math.cos(angle) * length;
-            maxTotalLength = Math.max(maxTotalLength, totalLength);
-
-            result.push({
-                size : size,
-                target : {
-                    origin : [0,0],
-                    transform : transform,
-                    target : node.render()
-                }
-            });
-        }
-
-        offset = [0,pivotLength,0];
-        for (var i = pivotIndex; i < nodes.length; i++){
             node = nodes[i];
             angle = angles[i];
 
@@ -101,11 +109,11 @@ define(function(require, exports, module) {
             var transform = Transform.thenMove(Transform.rotateX(angle), offset);
 
             result.push({
-                size : size,
-                target : {
-                    origin : [0,0],
-                    transform : transform,
-                    target : node.render()
+                size: size,
+                target: {
+                    origin: [0, 0],
+                    transform: transform,
+                    target: node.render()
                 }
             });
 
@@ -117,11 +125,18 @@ define(function(require, exports, module) {
 
         }
 
+        if (i === pivotIndex) pivotOffset = offset.slice();
+
         totalLength = Math.max(maxTotalLength, totalLength);
         this._size = [size[0], totalLength];
 
         return {
-            size : this._size,
+//            size : this._size,
+            transform : Transform.translate(
+                -this._initPivotOffset[0] - pivotOffset[0],
+                -this._initPivotOffset[1] - pivotOffset[1],
+                -this._initPivotOffset[2] - pivotOffset[2]
+            ),
             target : result
         };
     };
